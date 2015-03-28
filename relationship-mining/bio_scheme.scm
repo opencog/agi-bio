bc5a8b5fb46b9a1475e2cbe980e1d378e859bc75
Eddie Monroe <eddiemonroe@gmail.com>
2015-03-28 17:28:34 +0000

    add required scheme file bio_scheme.scm

---
+++ relationship-mining/bio_scheme.scm
@@ -0,0 +1,67 @@
+(ConceptNode "hello world")
+
+
+
+
+
+(define pattern_match_go_terms
+    (BindLink
+        (VariableNode "$go")
+        (ImplicationLink
+            (InheritanceLink
+                (VariableNode "$go")
+                (ConceptNode "GO_term"))
+            (VariableNode "$go"))))
+
+
+
+(define get_go_terms
+    (cog-outgoing-set (cog-bind pattern_match_go_terms)))
+
+
+
+(define (get_inheritance_child_nodes parent)
+    (cog-bind
+        (BindLink
+            (ListLink
+                (VariableNode "$child"))
+            (ImplicationLink
+                ;(AndLink
+                    (InheritanceLink
+                        (ConceptNode parent)
+                        (VariableNode "$child"))
+                ;    (NotLink
+                ;        (EquivalenceLink
+                ;            (VariableNode "$child")
+                ;            (ConceptNode "GO_term"))))
+                (VariableNode "$child")))))
+
+
+
+
+(define (get_members_of setname)
+    (cog-bind
+        (BindLink
+            (VariableNode "$member") 
+            (ImplicationLink
+                (MemberLink
+                    (VariableNode "$member")
+                    (ConceptNode setname))
+                (VariableNode "$member")))))
+
+
+(define (get_sets_for_member membername)
+    (cog-bind
+        (BindLink
+            (VariableNode "$setname")
+            (ImplicationLink
+                (MemberLink
+                    (GeneNode membername)
+                    (VariableNode "$setname"))
+                (VariableNode "$setname")))))
+
+
+
+
+(define (test something)
+    (display something))
