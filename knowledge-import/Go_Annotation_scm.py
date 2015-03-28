ca79b6c5ee3d3d58b6006791849a5121a3931259
Eddie Monroe <eddiemonroe@gmail.com>
2015-03-28 18:20:20 +0000

    changing GO annotation representation to memberlink

--- knowledge-import/Go_Annotation_scm.py
+++ knowledge-import/Go_Annotation_scm.py
@@ -17,6 +17,16 @@ lines_annotate = lines[line_no[0] -1 : len(lines)]
 
 ###### function to write on file
 
+def memberLink(gene, goID, qualifier):
+    if qualifier == 'NOT':
+        f_annotation.write("(MemberLink (stv 0.0 1.0)\n")
+    else:
+        f_annotation.write("(MemberLink\n")
+    f_annotation.write("\t(GeneNode \"" + gene + "\")\n")
+    f_annotation.write("\t(ConceptNode \"GO:" + goID + "\"))\n")
+
+# previous representation with evaluation link
+# using memberlink representation above now instead
 def evaLink(node1 , node2, qualifier):
     if qualifier == 'NOT' :
 	f_annotation.write("(EvaluationLink (stv 0.0 0.0)\n")
@@ -42,7 +52,8 @@ for l in lines_annotate:
     db_object_symbol =l.split('\t')[2]
     go_id = (l.split('\t')[4]).split(':')[1]
     qualifier = l.split('\t')[3]
-    evaLink(db_object_symbol,go_id, qualifier)
+    memberLink(db_object_symbol,go_id,qualifier)
+    #evaLink(db_object_symbol,go_id, qualifier)
 
 #close files
 f.close()
