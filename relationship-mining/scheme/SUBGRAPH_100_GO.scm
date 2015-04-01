ab3fddd2d5af7cda760f683b11347381477a9c33
Eddie Monroe <eddiemonroe@gmail.com>
2015-04-01 20:33:47 +0000

    add connected subgraphs for experiments

---
+++ relationship-mining/scheme/SUBGRAPH_100_GO.scm
@@ -0,0 +1,476 @@
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "ABL1") ; [763062]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [763137]
+
+(GeneNode "V9GYV3") ; [986370]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "V9GYV3") ; [986370]
+  (ConceptNode "GO:0050852") ; [439514]
+) ; [986371]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "V9GYV3") ; [986370]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [986372]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B lymphocyte receptor signalling pathway") ; [439556]
+) ; [439557]
+
+(EvaluationLink (stv 1.000000 0.000000)
+  (PredicateNode "GO_synonym_EXACT") ; [19]
+  (ListLink (stv 1.000000 0.000000)
+    (ConceptNode "GO:0050853") ; [439543]
+    (ConceptNode "B lymphocyte receptor signalling pathway") ; [439556]
+  ) ; [439557]
+) ; [439558]
+
+(ConceptNode "B lymphocyte receptor signaling pathway") ; [439553]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B-cell receptor signaling pathway") ; [439559]
+) ; [439560]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0002712") ; [30168]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1091372]
+
+(ConceptNode "B-cell receptor signalling pathway") ; [439562]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B-cell receptor signalling pathway") ; [439562]
+) ; [439563]
+
+(EvaluationLink (stv 1.000000 0.000000)
+  (PredicateNode "GO_synonym_EXACT") ; [19]
+  (ListLink (stv 1.000000 0.000000)
+    (ConceptNode "GO:0050853") ; [439543]
+    (ConceptNode "B-cell receptor signalling pathway") ; [439562]
+  ) ; [439563]
+) ; [439564]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B lymphocyte receptor signaling pathway") ; [439553]
+) ; [439554]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B-lymphocyte receptor signaling pathway") ; [439565]
+) ; [439566]
+
+(EvaluationLink (stv 1.000000 0.000000)
+  (PredicateNode "GO_synonym_EXACT") ; [19]
+  (ListLink (stv 1.000000 0.000000)
+    (ConceptNode "GO:0050853") ; [439543]
+    (ConceptNode "B-lymphocyte receptor signaling pathway") ; [439565]
+  ) ; [439566]
+) ; [439567]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B-lymphocyte receptor signalling pathway") ; [439568]
+) ; [439569]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "NCKAP1L") ; [834015]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [834051]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "LIME1") ; [950034]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [950039]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "CD38") ; [805628]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [805656]
+
+(ConceptNode "B lymphocyte receptor signalling pathway") ; [439556]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "BCAR1") ; [835839]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [835873]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0003138") ; [38934]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1211175]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "LYN") ; [775375]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [775464]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "BLK") ; [828188]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [828202]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "KLHL6") ; [918312]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [918315]
+
+(MemberLink (stv 0.000000 1.000000)
+  (GeneNode "BMX") ; [829714]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [829740]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0071226") ; [515453]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1056814]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "BTK") ; [851974]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [852019]
+
+(ConceptNode "B-lymphocyte receptor signaling pathway") ; [439565]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "CTLA4") ; [790565]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [790581]
+
+(EvaluationLink (stv 1.000000 0.000000)
+  (PredicateNode "GO_synonym_EXACT") ; [19]
+  (ListLink (stv 1.000000 0.000000)
+    (ConceptNode "GO:0050853") ; [439543]
+    (ConceptNode "B-cell receptor signaling pathway") ; [439559]
+  ) ; [439560]
+) ; [439561]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "PIK3CD") ; [735758]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [735802]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0005757") ; [77729]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1065279]
+
+(PredicateNode "GO_synonym_EXACT") ; [19]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0070447") ; [504231]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1038913]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "PLEKHA1") ; [953390]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [953410]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:1990117") ; [700985]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [701001]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "PRKCB") ; [772581]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [772621]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "LCK") ; [772890]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [772944]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "RFTN1") ; [867145]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [867161]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "PLCG2") ; [791105]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [791131]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0035984") ; [297331]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1110621]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0046671") ; [386157]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1119586]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "NFAM1") ; [908887]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [908899]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0043375") ; [333098]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1043557]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "MEF2C") ; [852225]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [852326]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0043378") ; [333204]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1090705]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0035509") ; [290426]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1198953]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0060025") ; [476587]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1080940]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0070667") ; [507666]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1150065]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "GO:0050851") ; [439503]
+) ; [439571]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0002721") ; [32626]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1037940]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0032848") ; [252257]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1212275]
+
+(ConceptNode "B-cell receptor signaling pathway") ; [439559]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0097144") ; [553909]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1206917]
+
+(SubsetLink (stv 0.600000 0.555556)
+  (ConceptNode "GO:0045588") ; [364689]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1218958]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "MNDA") ; [817284]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [817295]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0043376") ; [333123]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1148305]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0002316") ; [25464]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [987291]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "PTPRC") ; [776829]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [776860]
+
+(MemberLink (stv 0.000000 1.000000)
+  (GeneNode "ITK") ; [854405]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [854430]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "TEC") ; [818824]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [818847]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0001777") ; [17033]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1060261]
+
+(SubsetLink (stv 0.666667 0.555556)
+  (ConceptNode "GO:0002238") ; [24192]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1175719]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "SYK") ; [820072]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [820136]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0019814") ; [197960]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1029290]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0006808") ; [93201]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1026476]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "LAT2") ; [946596]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [946609]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0001865") ; [18525]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1189042]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0003185") ; [39551]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1172595]
+
+(GeneNode "ABL1") ; [763062]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0032633") ; [247396]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [997561]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0006991") ; [95393]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1208252]
+
+(MemberLink (stv 0.000000 1.000000)
+  (GeneNode "TXK") ; [818849]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [818878]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0002333") ; [25727]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1076673]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0019815") ; [197968]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1221579]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "BCL2") ; [781394]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [781508]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "NFATC2") ; [860844]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [860871]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "VAV3") ; [973480]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [973513]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0035983") ; [297320]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1223114]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0002363") ; [26252]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1089739]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "PTPN6") ; [806060]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [806098]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "CD19") ; [788932]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [788947]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0032976") ; [254675]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1048020]
+
+(EvaluationLink (stv 1.000000 0.000000)
+  (PredicateNode "GO_synonym_EXACT") ; [19]
+  (ListLink (stv 1.000000 0.000000)
+    (ConceptNode "GO:0050853") ; [439543]
+    (ConceptNode "B lymphocyte receptor signaling pathway") ; [439553]
+  ) ; [439554]
+) ; [439555]
+
+(ConceptNode "GO:0002712") ; [30168]
+
+(ConceptNode "GO:0050852") ; [439514]
+
+(SubsetLink (stv 0.600000 0.555556)
+  (ConceptNode "GO:0050861") ; [439801]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1033180]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:1902512") ; [644623]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1127133]
+
+(GeneNode "NCKAP1L") ; [834015]
+
+(GeneNode "PRKCB") ; [772581]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "CD79B") ; [816351]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [816363]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "MAPK1") ; [805273]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [805362]
+
+(MemberLink (stv 1.000000 0.000000)
+  (GeneNode "CD79A") ; [784105]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [784117]
+
+(SubsetLink (stv 1.000000 0.555556)
+  (ConceptNode "GO:0002332") ; [25707]
+  (ConceptNode "GO:0050853") ; [439543]
+) ; [1176310]
+
+(ConceptNode "GO:0050853") ; [439543]
+
+(InheritanceLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "GO_term") ; [4]
+) ; [439544]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B cell receptor signaling pathway") ; [439545]
+) ; [439546]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "biological_process") ; [16]
+) ; [439548]
+
+(ListLink (stv 1.000000 0.000000)
+  (ConceptNode "GO:0050853") ; [439543]
+  (ConceptNode "B cell receptor signalling pathway") ; [439550]
+) ; [439551]
+
