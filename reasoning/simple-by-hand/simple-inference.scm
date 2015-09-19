56b165d5496a442ad24f991e9af3ab5421fa7a9f
Eddie Monroe <eddiemonroe@gmail.com>
2015-09-18 20:03:57 -0400

    remove some unused code

--- reasoning/simple-by-hand/simple-inference.scm
+++ reasoning/simple-by-hand/simple-inference.scm
@@ -7,46 +7,8 @@
 ;; Utility Functions ;;
 ;;;;;;;;;;;;;;;;;;;;;;;
 
-; atomspace populating helpers
-(define knowledge-dir "../../../bio-data/scheme-representations/")
-(define subgraph-dir (string-append knowledge-dir "subgraphs/"))
-
-(define (loadf f)
-    (load (string-append knowledge-dir f)))
-(define (loadsub f) (load (string-append subgraph-dir f)))
-
-(define (loadGO1K)
-    (loadf "subgraphs/subgraph_1K_GO.scm")
-    (set_bio_tvs))
-(define (load1K)
-    (loadf "subgraphs/subgraph_1K.scm")
-    (set_bio_tvs))
-
-(define (set_bio_tvs)
-    (set_default_gene_tvs)
-    (set_default_concept_tvs))
-
-
-; set the truth value for GeneNodes
-(define (set_gene_tvs strength confidence)
-    (let ([genes (cog-get-atoms 'GeneNode)])
-       (for-each (lambda (gene)
-            (cog-set-tv! gene (cog-new-stv strength confidence)))
-            genes)))
-
-;set truth values for all ConceptNodes
-(define (set_concept_tvs strength confidence)
-    (let ([nodes (cog-get-atoms 'ConceptNode)])
-        (for-each (lambda (node)
-            (cog-set-tv! node (cog-new-stv strength confidence)))
-            nodes)))
-
-;set default truth values for all GeneNodes or all ConceptNodes
-(define (set_default_gene_tvs) (set_gene_tvs .00001 .9))
-(define (set_default_concept_tvs) (set_concept_tvs .001 .9))
-
 (define (display-atom label atom)
-    (display label)(display ": ")(display atom))
+    (newline)(display label)(display ": \n")(display atom))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load the atomspace and rules ;;
