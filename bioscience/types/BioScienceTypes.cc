2b8b5b9dc95d702c594624f8132a9920224b39b3
mjsd <mjsduncan@gmail.com>
2017-06-20 17:17:32 -0400

    update include directory for "Module.h"

--- bioscience/types/BioScienceTypes.cc
+++ bioscience/types/BioScienceTypes.cc
@@ -20,7 +20,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include <opencog/server/Module.h>
+#include <opencog/cogserver/server/Module.h>
 #include "bioscience/types/atom_types.definitions"
 
 #define INHERITANCE_FILE "bioscience/types/atom_types.inheritance"
