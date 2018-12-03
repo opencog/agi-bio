73e260af8a6478d2623ede5c81dab0e5aaf806ee
kasim <se.kasim.ebrahim@gmail.com>
2018-12-03 18:17:07 +0300

    Correct include path proto to value

--- bioscience/types/BioScienceTypes.cc
+++ bioscience/types/BioScienceTypes.cc
@@ -25,4 +25,4 @@
 #define INHERITANCE_FILE "bioscience/types/atom_types.inheritance"
 #define INITNAME bioscience_types_init
 
-#include <opencog/atoms/proto/atom_types.cc>
+#include <opencog/atoms/value/atom_types.cc>
