7c10b18002808642c7f1975d2fe8f1329da8b0dc
Hedra <tanksh24@gmail.com>
2020-03-09 18:47:03 -0400

    Includes the GGI

--- knowledge-import/SNET/string_PPI.py
+++ knowledge-import/SNET/string_PPI.py
@@ -12,13 +12,20 @@ import datetime
 source = "https://stringdb-static.org/download/protein.actions.v11.0/9606.protein.actions.v11.0.txt.gz"
 mapping = "https://string-db.org/mapping_files/uniprot/human.uniprot_2_string.2018.tsv.gz"
 
-def evaLink(term1, term2, predicate, link_type="ListLink"):
+def evaLink(term1, term2, predicate, link_type="ListLink", ppi=True):
     if not (str(term1) == "nan" or str(term2) == 'nan'):
-        return("(EvaluationLink \n" +
+        if ppi:
+            return("(EvaluationLink \n" +
+                "\t (PredicateNode \""+ predicate + "\")\n" +
+                "\t (" + link_type + " \n" +
+                "\t\t (MoleculeNode" + " \"Uniprot:" + term1 + "\")\n" +
+                "\t\t (MoleculeNode" + " \"Uniprot:" + term2 + "\")))\n" )
+        else:
+            return("(EvaluationLink \n" +
             "\t (PredicateNode \""+ predicate + "\")\n" +
             "\t (" + link_type + " \n" +
-            "\t\t (MoleculeNode" + " \"Uniprot:" + term1 + "\")\n" +
-            "\t\t (MoleculeNode" + " \"Uniprot:" + term2 + "\")))\n" )
+            "\t\t (GeneNode" + " \"" + term1.upper() + "\")\n" +
+            "\t\t (GeneNode" + " \"" + term2.upper() + "\")))\n" )
     else:
         return ""
 def import_string():
@@ -39,29 +46,56 @@ def import_string():
     print(len(df_data))
     notmapped = []
     print("********** " + str(datetime.datetime.now()))
-    with open("dataset/string_ppi.scm", "w") as f:
+    with open("dataset/string_ppi.scm", "w") as f, open('dataset/string_ggi.scm', 'w') as g:
         for i in range(len(df_data)):
             try:
                 prot1 = df_data.iloc[i]['item_id_a']
                 prot2 = df_data.iloc[i]['item_id_b']
                 mode = df_data.iloc[i]['mode']
                 is_directional = df_data.iloc[i]['is_directional'] # is_directional - describes if the diractionality of the particular interaction is known.
+                a_is_acting = df_data.iloc[i]['a_is_acting'] # the directionality of the action if applicable ('t' gives that item_id_a is acting upon item_id_b)               
+                """
+                If the directionality of the interaction is true and a is acting, use ListLink and keep the order. Otherwise use SetLink
+                Example:
+                item_id_a   item_id_b   mode    is_directional  a_is_acting
+                ENSP00000000233	ENSP00000216366 reaction    f   f   
+                <=> EvaluationLink 
+                        PredicateNode "reaction"
+                        SetLink ENSP00000000233 ENSP00000216366
 
-                if prot1 in mapping_dict.keys():
+                ENSP00000000233	ENSP00000216366 reaction    t   f
+                <=> EvaluationLink 
+                        PredicateNode "reaction"
+                        ListLink ENSP00000216366 ENSP00000000233
+                ENSP00000000233	ENSP00000216366	reaction    t   t
+                <=> EvaluationLink 
+                        PredicateNode "reaction"
+                        ListLink ENSP00000000233 ENSP00000216366
+                """
+                if prot1 in mapping_dict.keys() and prot2 in mapping_dict.keys():
                     prot1 = mapping_dict[prot1]
-                else:
-                    notmapped.append(prot1)
-                if prot2 in mapping_dict.keys():
                     prot2 = mapping_dict[prot2]
                 else:
-                    notmapped.append(prot2)
+                    if not prot1 in mapping_dict.keys():
+                        notmapped.append(prot1)
+                    else:
+                        notmapped.append(prot2)
+                    continue
                 
-                prot1 = prot1.split("|")[0]
-                prot2 = prot2.split("|")[0]
+                protein1 = prot1.split("|")[0]
+                gene1 = prot1.split("|")[1].split("_")[0] 
+                protein2 = prot2.split("|")[0]
+                gene2 = prot2.split("|")[1].split("_")[0]
                 if is_directional is "f":
-                   f.write(evaLink(prot1, prot2, mode,link_type="SetLink")) 
+                   f.write(evaLink(protein1, protein2, mode,link_type="SetLink"))
+                   g.write(evaLink(gene1, gene2, mode,link_type="SetLink", ppi=False))  
                 else:
-                   f.write(evaLink(prot1, prot2, mode))
+                    if a_is_acting is "t": 
+                        f.write(evaLink(protein1, protein2, mode))
+                        g.write(evaLink(gene1, gene2, mode, ppi=False))
+                    else:
+                        f.write(evaLink(protein2, protein1, mode))
+                        g.write(evaLink(gene2, gene1, mode, ppi=False))
 
             except Exception as e:
                 print(e)
